b9166f1030b7789415e99ad8a5e8c3868f977896
Nicolai Haug <nicoha@student.matnat.uio.no>
2018-08-27 17:43:26 +0200

    Test commit

---
+++ Project1/test.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld!
+//
+//  Created by Nicolai Haug on 24/06/2018.
+//  Copyright © 2018 Nicolai Haug. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+        std::cout << "Hello world!" << std::endl;
+        std::cin.get();
+}
