061decd3cde136fe16f2749de012b07847883ef9
Nicolai Haug <nicoha@student.matnat.uio.no>
2018-08-27 18:53:31 +0200

    Added Python template file

---
+++ Project1/mal.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set fontsizes in figures
+params = {'legend.fontsize': 'large',
+          'axes.labelsize': 'large',
+          'axes.titlesize': 'large',
+          'xtick.labelsize': 'large',
+          'ytick.labelsize': 'large'}
+plt.rcParams.update(params)
